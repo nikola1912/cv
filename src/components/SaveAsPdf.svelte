27e6aad8999e18f1e252b12af5955617386b8f30
Nikola Jovanovic <nikola.jovanovic@montecha.com>
2022-04-10 01:41:06 +0200

    WIP

--- src/components/SaveAsPdf.svelte
+++ src/components/SaveAsPdf.svelte
@@ -1,9 +1,17 @@
 <script>
+	import { browser } from '$app/env';
+
 	const SEJDA_HTML_TO_PDF_URL = 'https://www.sejda.com/html-to-pdf';
+	const EXPORT_PDF_ENDPOINT = 'export-pdf';
+
+	if (browser) {
+		const urlParams = new URLSearchParams(window.location.search);
+		console.log(urlParams);
+	}
 
 	function saveAsPdf(event) {
 		event.preventDefault();
-		const pageUrl = encodeURIComponent(window.location.href);
+		const pageUrl = encodeURIComponent(window.location.href + `?${EXPORT_PDF_ENDPOINT}=true`);
 		window.open(SEJDA_HTML_TO_PDF_URL + '?save-link=' + pageUrl);
 	}
 </script>
@@ -14,5 +22,6 @@
 		outline: 0 none !important;
 		border: none;
 		font-size: 1.1em;
+		color: inherit;
 	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
 import path from 'path';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 
 const GITHUB_REPO = '/cv';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		appDir: 'internal',
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? GITHUB_REPO : ''
-		},
+		// appDir: 'internal',
+		// paths: {
+		// 	base: process.env.NODE_ENV === 'production' ? GITHUB_REPO : ''
+		// },
 		target: '#svelte',
-		adapter: adapter(),
+		// adapter: adapter(),
 		vite: {
 			resolve: {
 				alias: {
